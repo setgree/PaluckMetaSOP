7f51a4cc14df7f6aa9834f4482167d71f9dc1769
setgree <setgree@gmail.com>
2024-01-03 16:08:45 -0500

    expected whitepsace

--- R/make_bib.R
+++ R/make_bib.R
@@ -11,32 +11,32 @@
 manage_references <- function(dat_with_dois, bib_file = './refs.bib') {
   # Check for required packages
   missing_packages <- character(0)
-  
+
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     missing_packages <- c(missing_packages, "'dplyr'")
   }
   if (!requireNamespace("RefManageR", quietly = TRUE)) {
     missing_packages <- c(missing_packages, "'RefManageR'")
   }
-  
+
   if (length(missing_packages) > 0) {
     stop(paste("Required package(s)", paste(missing_packages, collapse = ", "), "is/are not installed."))
   }
-  
+
   # Write bibs into bib file
-  for(entry in dat_with_dois$doi){
+  for (entry in dat_with_dois$doi){
     RefManageR::WriteBib(bib = RefManageR::GetBibEntryWithDOI(doi = entry),
                          file = bib_file,
                          append = TRUE)
   }
-  
+
   # Function to add DOI to the BibTeX file
   add_doi_to_bib <- function(new_doi) {
     RefManageR::WriteBib(bib = RefManageR::GetBibEntryWithDOI(new_doi),
                          file = bib_file,
-                         append = TRUE) 
+                         append = TRUE)
   }
-  
+
   # Return the function to add DOIs to the BibTeX file
   return(list(add_doi_to_bib = add_doi_to_bib))
 }
