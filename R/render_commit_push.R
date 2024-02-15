35cdbea09df1e17477c81457e58d052b7927b566
Seth Green <setgree@gmail.com>
2024-02-15 12:18:26 -0500

    revised intro to actually explain meta science a bit

--- R/render_commit_push.R
+++ R/render_commit_push.R
@@ -1,9 +1,10 @@
 #' Render, commit, and push changes for an R Markdown document
 #'
 #' This function renders (knits) an R Markdown document, performs a Git
-#' commit with a provided commit message, and pushes the changes to the remote repository.
+#' commit with a provided commit message, and optionally pushes the changes to the remote repository.
 #'
 #' @param commit_message A commit message
+#' @param skip_push Logical indicating whether to skip the Git push step. Default is TRUE.
 #' @importFrom rmarkdown render
 #' @importFrom here here
 #' @return None
@@ -12,7 +13,7 @@
 #' render_commit_push("Update manuscript")
 #' }
 #' @export
-render_commit_push <- function(commit_message = "update manuscript") {
+render_commit_push <- function(commit_message = "update manuscript", skip_push = TRUE) {
   # Step 1: Find R Markdown files in the project directory and subdirectories
   rmd_files <- list.files(path = here::here(), pattern = "\\.Rmd$", recursive = TRUE, full.names = TRUE)
 
@@ -27,6 +28,10 @@ render_commit_push <- function(commit_message = "update manuscript") {
   system("git add ./*")
   system(paste("git commit -m", shQuote(commit_message)))
 
-  # Step 4: Git push
-  system("git push")
+  # Step 4: Git push (if skip_push is FALSE, otherwise skip)
+  if (!skip_push) {
+    print("Git push skipped.")
+  } else {
+    system("git push")
+  }
 }
