39472ac30afec2063acf32b449a15a6026a71948
Seth Green <setgree@gmail.com>
2024-04-18 10:22:58 -0400

    ok this took many false starts but I'm finally very happy with the structure and form of the d and var_d vignette

--- R/d_calc.R
+++ R/d_calc.R
@@ -41,20 +41,17 @@
 
 d_calc <- function(stat_type, stat, sample_sd, n_t, n_c) {
   # Calculate Cohen's D or Glass's $\Delta$ based on effect size and sample SD
-  if (stat_type == "d_i_d" ||
-      stat_type == "d_i_m" ||
-      stat_type == "reg_coef" ||
-      stat_type == "regression" ||
-      stat_type == "beta") {
-    d <- round(stat / sample_sd, digits = 3)
-  } else if (stat_type == "d" ||
-             stat_type == "s_m_d") {
+  if (stat_type == "d" ||
+      stat_type == "s_m_d") {
     # Directly use the reported change of SDs
     d <- stat
-  } else if (stat_type == "unspecified_null" ||
-             stat_type == "unspecified null") {
-    # Set an 'unspecified null' result to a default small value
-    d <- 0.01
+  }
+  else if (stat_type == "d_i_m" ||
+           stat_type == "d_i_d" ||
+           stat_type == "reg_coef" ||
+           stat_type == "regression" ||
+           stat_type == "beta") {
+    d <- round(stat / sample_sd, digits = 3)
   } else if (stat_type == "t_test" ||
              stat_type == "T-test") {
     # Calculate Cohen's D for t test
@@ -78,7 +75,13 @@ d_calc <- function(stat_type, stat, sample_sd, n_t, n_c) {
     # is $p(1-p)$; so the estimator in total is
     #  $$\Delta = \frac{p_{1} - p_{2}}{\sqrt{p_{2} * (1 - p_{2)}}}$$
     d = stat / (sqrt(sample_sd * (1 - sample_sd)))
-  } else {
+    }
+  else if (stat_type == "unspecified_null" ||
+                 stat_type == "unspecified null") {
+    # Set an 'unspecified null' result to a default small value
+    d <- 0.01
+  }
+  else {
     # Default value if stat_type is unrecognized
     d <- NA
   }
