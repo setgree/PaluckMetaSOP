fafcd07aef5946659bd6f86071c882ccdaaa2238
Seth Green <setgree@gmail.com>
2024-05-01 10:46:28 -0400

    condensing vignettes and eliminated a function

--- R/map_robust.R
+++ R/map_robust.R
@@ -16,21 +16,26 @@
 #'
 #' @param x The dataset or subset to perform meta-analysis on.
 #' @return A tibble/data frame with meta-analysis results.
+#' @examples
+#' \dontrun{
+#' PaluckMetaSOP::sv_data |> map_robust()
+#' PaluckMetaSOP::sv_data |> split(~behavior_type) |> purrr::map(map_robust) |> dplyr::bind_rows(.id = "behavior_type")
+#' }
 #' @export
 map_robust <- function(x) {
   # Function to format p-values without leading zeroes or unnecessary detail
   # if they're very small
   format_pval <- function(p) {
     if (is.na(p)) {
-      NA
+      "NA     "  # Using space padding to align with other p-values
     } else if (p < 0.0001) {
-      "< .0001"
+      "<.0001"  # Special case without space for very small p-values
     } else {
       formatted_p <- formatC(p, format = "f", digits = 4)
       if (substr(formatted_p, 1, 1) == "0") {
-        substr(formatted_p, 2, nchar(formatted_p))
+        paste0(" ", substr(formatted_p, 2, nchar(formatted_p)))  # Add a leading space for non-zero leading p-values
       } else {
-        formatted_p
+        paste0(" ", formatted_p)  # Ensure leading space for all other cases
       }
     }
   }
@@ -42,20 +47,24 @@ map_robust <- function(x) {
     pval <- NA
 
     # Return these values in a tibble
-    return(tibble::tibble(N_unique = nrow(x), Delta = Delta, se = se, pval = pval))
+    return(data.frame(N_unique = nrow(x),
+                      Delta = Delta,
+                      se = se,
+                      pval = pval))
   } else {
     # Perform robust meta-analysis using metafor::robust function
     result <- metafor::robust(x = metafor::rma(yi = x$d, vi = x$var_d), cluster = x$unique_study_id)
+    num_studies <- length(x$unique_study_id)
 
     # Extract relevant results and format them into a tibble/data frame
     output <- data.frame(
-      N_unique = nrow(x),
+      N_observations = result$k,
+      N_studies = result$n,
       Delta = round(result$beta, 4),
       se = round(result$se, 4),
-      pval = format_pval(result$pval)) |>
+      pval = noquote(format_pval(result$pval))) |>
       tibble::as_tibble()
 
     return(output)
   }
 }
-
