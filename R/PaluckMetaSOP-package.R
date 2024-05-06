4e94c6fc54c8ace510769d1b2808fd791242a1c8
Seth Green <setgree@gmail.com>
2024-05-06 12:22:44 -0400

    working on the 'top line' documentation for the package

--- R/PaluckMetaSOP-package.R
+++ R/PaluckMetaSOP-package.R
@@ -6,23 +6,29 @@
 #' @title PaluckMetaSOP: Standard Operating Procedures for Meta-Analysis
 #' @description
 #'
-#' This package currently includes six functions: \code{\link{d_calc}}, \code{\link{map_robust}}, \code{\link{study_count}},
-#' \code{\link{sum_lm}}, \code{\link{sum_tab}}, and \code{\link{var_d_calc}}.
+#' It comprises six functions, two datasets, and four vignettes.
 #'
-#' It also includes two datasets from the Paluck Lab's meta-analyses:
-#' \code{\link{contact_data}} and  \code{\link{sv_data}}. These datasets
-#' are used in the vingettes and function documentation for as examples of
-#' how to use the functions.
+#' The main documentation for this package is in its vignettes.
+#' [The first](vignettes/overview-of-vignettes.html), provides an overview
+#' of the next three  vignettes and where they fit into the overall structure
+#' of a meta-analysis. [The second](vignettes/d-calc-vignette.html) is about
+#' converting a study's results into an estimate of standardized mean
+#' difference, variance, and standard error.
+#' [The third](vignettes/meta-analysis-vignette.html) is about actually
+#' doing meta-analysis. [The fourth](vignettes/writing-metas-vignette.html)
+#' is about writing meta-analysis papers.
 #'
-#' The main documentation for this package is in its vignettes. The first
-#' provides an overview of the meta-analytic procedure and what these functions
-#' are meant to help with. The second is about converting a study's results into
-#' an estimate of standardized mean difference. The third is about doing meta-analysis.
-#' The fourth is about how we write meta-analysis papers.
+#' The six functions are \code{\link{d_calc}}, \code{\link{map_robust}},
+#'  \code{\link{study_count}}, \code{\link{sum_lm}}, \code{\link{sum_tab}}, and
+#'  \code{\link{var_d_calc}}. The two datasets are \code{\link{contact_data}}
+#'  and \code{\link{sv_data}}. These datasets are used in the vignettes and
+#'  function documentation as examples. They come from our papers
+#' 'The Contact Hypothesis Re-evaluated' and 'Preventing Sexual Violence
+#' —A Behavioral Problem Without a Behaviorally-Informed Solution', respectively.
 #'
-#' Make sure you install with vignettes included:
-#' `remotes::install('setgree/PaluckMetaSOP', build_vignettes = T)`
-#' and then run `browseVignettes('PaluckMetaSOP')`.
+#' Please install this package with vignettes included via
+#' `remotes::install('setgree/PaluckMetaSOP', build_vignettes = T)`.
+#' You can then browse the vignettes with  `browseVignettes('PaluckMetaSOP')`.
 #'
 #' @name PaluckMetaSOP-package
 #' @aliases PaluckMetaSOP
